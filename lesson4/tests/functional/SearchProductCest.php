620de05f657d88b5c56cb22c21bcff33e127c4bb
Iliyas <IAripkul@Homecredit.kz>
2021-05-24 02:47:50 +0600

    created functional test

---
+++ lesson4/tests/functional/SearchProductCest.php
@@ -0,0 +1,17 @@
+<?php
+
+class SearchProductCest
+{
+
+    // tests
+    public function clickSearchButtonAndFindProducts(FunctionalTester $I)
+    {
+       $I->amOnPage("");
+       $I->seeElement("[class='search_query form-control']");
+       $I->fillField("[class='search_query form-control']", "Printed dress");
+       $I->seeElement("[name='submit_search']");
+       $I->click("[name='submit_search']");
+       $I->seeNumberOfElements("[class='product-image-container']", 5);
+    }
+
+}
