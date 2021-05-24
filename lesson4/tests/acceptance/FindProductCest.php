530c55bd28c99e751826ed825459308f001de0d9
Әріпқұл Ілияс Жеңісбекұлы <iliyas.aripkul@kaspi.kz>
2021-05-24 15:12:52 +0600

    test

---
+++ lesson4/tests/acceptance/FindProductCest.php
@@ -0,0 +1,22 @@
+<?php
+
+class FindProductCest
+{
+    /**
+    * Найти товар, нажать на Быстрый просмотр и проверить название товара
+    */
+    public function FindProductAndQuickVieW(AcceptanceTester $I)
+    {
+        $I->amOnPage("");
+        $I->waitForElementVisible("[title='Blouse']");
+        $I->seeElement("#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view");
+
+        codecept_debug($I->grabTextFrom("#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view"));
+
+        $I->click("#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view");
+
+        $I->seeElement("//h1[contains(text(),'Blouse')]");
+
+        codecept_debug($I->grabTextFrom("//h1[contains(text(),'Blouse')]"));
+    }
+}
