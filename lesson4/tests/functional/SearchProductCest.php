38ce4f1347ebb5222f8d0b180955038565067155
Әріпқұл Ілияс Жеңісбекұлы <iliyas.aripkul@kaspi.kz>
2021-05-27 11:04:55 +0600

    added varibles for XPath and CSS

--- lesson4/tests/functional/SearchProductCest.php
+++ lesson4/tests/functional/SearchProductCest.php
@@ -6,6 +6,13 @@ class SearchProductCest
     // tests
     public function clickSearchButtonAndFindProducts(FunctionalTester $I)
     {
+       $searchBoxCss="#search_query_top";
+       $searchBoxXPath="//input[@class='search_query form-control ac_input']";
+       $submitSearchButtonCss = "[name='submit_search']";
+       $submitSearchButtonXPath = "//button[@type='submit' and @name='submit_search']";
+       $productImagesCss = "[class='product-image-container']";
+       $productImagesXPath = "//div[@class='product-image-container']"
+       
        $I->amOnPage(""); // изменение для пула
        $I->seeElement("[class='search_query form-control']");
        $I->fillField("[class='search_query form-control']", "Printed dress");
