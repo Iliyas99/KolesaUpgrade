4dff8a782550f944fdf2bbaf7ff260f86844e615
Iliyas <IAripkul@Homecredit.kz>
2021-06-16 22:08:49 +0600

    переместил файл в корневую

---
+++ tests/acceptance/GridAndListSwitcherCest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Page\Acceptance\MainPage;
+
+/**
+ * Класс для проверки Grid и List
+ */
+class GridAndListSwitcherCest
+{
+    /**
+     * Проверяем, что список продуктов переключается между Grid/List
+     */
+
+    public function checkGridAndListSwitchMode(AcceptanceTester $I)
+    {
+        $mainPage = new MainPage($I);
+        
+        $I->amOnPage(MainPage::$URL);
+
+        $mainPage->findDressesButton()->moveMouseToDressesButton()->clickSummerDressesButton();
+        /*$I->seeElement(MainPage::$dressesButton);
+        $I->moveMouseOver(MainPage::$dressesButton);
+        $I->click(MainPage::$summerDressesButton);*/
+
+        $mainPage->waitForGridButtonAppear();
+
+        $mainPage->validateGridButtonSelected()->validateProductsInGridMode();
+        /*$I->seeElement(MainPage::$gridButton);
+        $I->seeElement(MainPage::$gridTable);*/
+
+        //$I->waitForElement(MainPage::$listButton, 3);
+
+        $mainPage->clickListModeButton()->validateProductsInListMode();
+        /*$I->click(MainPage::$listButton);
+        $I->seeElement(MainPage::$listTable);*/
+    }
+}
---
+++ tests/acceptance/LoginCest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Page\Acceptance\LoginPage;
+use Page\Acceptance\MainPage;
+
+/**
+ * Класс для проверки Grid и List
+ */
+class LoginCest
+{
+    /**
+     * Проверяем, что список продуктов переключается между Grid/List
+     */
+
+    public function checkErrorInLogin(AcceptanceTester $I)
+    {
+        $loginPage = new LoginPage($I);
+        $I->amOnPage('');
+
+        $loginPage->fillNameField()->fillPasswordField()->clickSubmit();
+        /*
+        $I->fillField("//input[@id='user-name']", "locked_out_user");
+        $I->fillField("//input[@id='password']", "secret_sauce");
+        $I->click("//input[@id='login-button']");*/
+
+        $loginPage->waitForErrorBoxAppear()->clickToDeleteErrorBox()->validateErrorBoxDisapear();
+        /*$I->waitForElement("//div[@class='error-message-container error']", 3);
+        $I->click("//button[@class='error-button']");
+        $I->dontSeeElement("//h3[@data-test='error']");*/
+    }
+}
